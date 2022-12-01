4b5a91c980a1ed9d3f087c6454d7b3b5ed76c360
Anthony <arovira@linxea.com>
2022-12-01 15:28:13 +0100

    done: 01

---
+++ src/01/index.js
@@ -0,0 +1,60 @@
+const fs = require("fs");
+path = require("path");
+filePath = path.join(__dirname, "input.txt");
+
+try {
+  const raw = fs.readFileSync(filePath, "utf8");
+  const data = raw.toString().split("\r\n");
+  let sumOfCalories = 0;
+  let highest = 0;
+  for (let i = 0; i < data.length; i++) {
+    if (!!data[i]) {
+      sumOfCalories += parseInt(data[i]);
+      if (sumOfCalories > highest) {
+        highest = sumOfCalories;
+      }
+    } else sumOfCalories = 0;
+  }
+  console.log(highest);
+} catch (error) {
+  console.error(error);
+}
+
+try {
+  const raw = fs.readFileSync(filePath, "utf8");
+  const data = raw.toString().split("\r\n");
+  let sumOfCalories = 0;
+  let highests = [0, 0, 0];
+  for (let i = 0; i < data.length; i++) {
+    if (!!data[i]) {
+      sumOfCalories += parseInt(data[i]);
+    } else {
+      let shouldReplace = false;
+      for (let j = 0; j < highests.length; j++) {
+        if (
+          sumOfCalories > highests[j] &&
+          !shouldReplace &&
+          !highests.includes(sumOfCalories)
+        ) {
+          shouldReplace = true;
+        }
+        if (shouldReplace) {
+          highests[j] = sumOfCalories;
+          shouldReplace = false;
+        }
+      }
+
+      highests.sort(function (a, b) {
+        return a - b;
+      });
+
+      sumOfCalories = 0;
+    }
+  }
+  const sum = highests.reduce((accumulator, value) => {
+    return accumulator + value;
+  }, 0);
+  console.log("sum ", sum);
+} catch (error) {
+  console.error(error);
+}
